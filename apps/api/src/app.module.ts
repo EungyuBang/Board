dc3b884a6439195039c27fe67d9230c6f6d5a49d
EungyuBang <q6516179@gmail.com>
2025-12-14 23:55:24 +0900

    feat: api 와꾸 잡기

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
-import { ExamModule } from './exam/exam.module';
-import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './prisma/prisma.module';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { PrismaModule } from "./prisma/prisma.module";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { PostModule } from "./post/post.module";
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env' }), PrismaModule, ExamModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: ".env", isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    PostModule,
+  ],
   controllers: [],
   providers: [],
 })
